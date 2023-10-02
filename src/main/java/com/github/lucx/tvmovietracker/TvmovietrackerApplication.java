0cc62658b709578aed5bdc545cca86bdf6cc0e3d
Lyuu17 <lyuu17@yandex.com>
2023-10-02 02:29:34 +0200

    Initial commit

---
+++ src/main/java/com/github/lucx/tvmovietracker/TvmovietrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucx.tvmovietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmovietrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmovietrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucx/tvmovietracker/TvmovietrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucx.tvmovietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmovietrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
