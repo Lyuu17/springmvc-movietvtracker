a6bf092bb8af877811f79219ff9f3c859b39b7c9
Lyuu17 <lyuu17@yandex.com>
2023-10-02 16:20:18 +0200

    Disable Spring Security autoconf for now

--- src/main/java/com/github/lucx/tvmovietracker/TvmovietrackerApplication.java
+++ src/main/java/com/github/lucx/tvmovietracker/TvmovietrackerApplication.java
@@ -2,12 +2,13 @@ package com.github.lucx.tvmovietracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TvmovietrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TvmovietrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TvmovietrackerApplication.class, args);
+    }
 
 }
